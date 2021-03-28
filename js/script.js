16efa8a0086cd99aa9fc726898f8f92d301dfccf
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-03-28 15:29:36 -0300

    add modal - ainda nao esta 100% certo

--- js/script.js
+++ js/script.js
@@ -3,3 +3,30 @@ const toggle = document.getElementById('toggle');
 toggle.addEventListener('click', () =>
   document.body.classList.toggle('show-nav')
 );
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
