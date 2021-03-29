a98aff90212eedd5d3d8781aa347d0c68ead2629
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-03-29 20:54:50 -0300

    pequenas alteracoes para o php funcionar

--- includes/header.php
+++ includes/header.php
@@ -7,17 +7,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="shortcut icon" href="img/mst.png">
+    <link rel="shortcut icon" href="/img/mst.png">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" rel="stylesheet" />
 
-    <link href="scss/style.css" rel="stylesheet">
-    <link href="css/login.css" rel="stylesheet">
-    <link href="css/adicionar-ficha.css" rel="stylesheet">
-    <link href="css/lista-fichas.css" rel="stylesheet">
-    <link href="css/graficos.css" rel="stylesheet">
-    <link href="css/tabelas.css" rel="stylesheet">
-    <link href="css/previsoes.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/login.css" rel="stylesheet">
+    <link href="/css/adicionar-ficha.css" rel="stylesheet">
+    <link href="/css/lista-fichas.css" rel="stylesheet">
+    <link href="/css/graficos.css" rel="stylesheet">
+    <link href="/css/tabelas.css" rel="stylesheet">
+    <link href="/css/previsoes.css" rel="stylesheet">
 
     <title>COOPTAR</title>
 
@@ -30,11 +30,11 @@
             <img src="/img/mst.png" alt="user" />
         </div>
         <ul>
-            <li><a href="lista-fichas.php">Fichas dos animais</a></li>
-            <li><a href="graficos.php">Gráficos</a></li>
-            <li><a href="previsoes.php">Previsões</a></li>
-            <li><a href="tabelas.php">Tabelas</a></li>
-            <li><a href="producao-leite.php">Adicionar produção de leite</a></li>
+            <li><a href="/lista-fichas.php">Fichas dos animais</a></li>
+            <li><a href="/graficos.php">Gráficos</a></li>
+            <li><a href="/previsoes.php">Previsões</a></li>
+            <li><a href="/tabelas.php">Tabelas</a></li>
+            <li><a href="/producao-leite.php">Adicionar produção de leite</a></li>
 
         </ul>
     </nav>
@@ -46,3 +46,4 @@
         <a class="logo-header" href="index.php"><img title="Logo do mst" src="/img/mst.png" /></a>
     </header>
 
+    <script src="/js/script.js"></script>
