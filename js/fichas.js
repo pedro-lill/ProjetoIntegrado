db5cfde802c40acbba930e2f15faca27453ae24c
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-04-17 20:07:40 -0300

    ajeitando js

--- js/fichas.js
+++ js/fichas.js
@@ -1,16 +1,14 @@
-
-var span_insem = document.querySelector("#close_insem");
-var span_medi = document.querySelector("#close_medi");
-var span_prod = document.querySelector("#close_prod");
-
 var hist_insem = document.querySelector("#button-historico-inseminacao");
 var modal_insem = document.getElementById("modal_insem");
+var span_insem = document.querySelector("#close_insem");
 
 var hist_prod = document.querySelector("#button-historico-producao");
 var modal_prod = document.getElementById("modal_prod");
+var span_prod = document.querySelector("#close_prod");
 
 var hist_medi = document.querySelector("#button-historico-medicacao");
 var modal_medi = document.getElementById("modal_medi");
+var span_medi = document.querySelector("#close_medi");
 
 
 hist_insem.onclick = function() {
