e46657cdb237bb3200228c22104c9db6f93c39ec
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-04-19 15:02:57 -0300

    alterando nome das paginas de login-index

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
-<?php require_once("includes/header.php");?>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<h1>TELA INICIAL</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/login.css">
+    <title>Login - COOPTAR</title>
+</head>
 
+<body>
 
+  <a class="logo-mst"><img title="Logo do mst" src="/img/mst.png"/></a>
 
+  <form  action="telaInicial.php" onsubmit="return validar()">
+    <div class="container-login">
+      <input type="text" placeholder="Enter Username" name="username" required>
+      <input type="password" placeholder="Enter Password" name="password" required>
+      <button class="button-form" type="submit" onclick="validacaoLogin()">Login</button>
+    </div>
+  </form>
+
+  <script src="/js/login.js"></script>
+
+</body>
+
+</html>
---
+++ telaInicial.php
@@ -0,0 +1,6 @@
+<?php require_once("includes/header.php");?>
+
+<h1>TELA INICIAL</h1>
+
+
+
