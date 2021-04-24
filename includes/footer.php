0cf2867d7596a30991027a0c981b1b3ea746dcd9
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-04-24 18:57:43 -0300

    add footer

---
+++ includes/footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="shortcut icon" href="/img/mst.png">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" rel="stylesheet" />
+    <link href="../css/footer.css" rel="stylesheet">
+    <title>COOPTAR</title>
+</head>
+
+<body>
+
+<footer>
+        <div class="container__footer">
+            <div class="container__text">
+                <span>COOPTAR - MST</span>
+                <span>Pontão - RS</span>
+                <span>Pedro Lill e Fabrício Romani</span>
+            </div>
+            <div class="container__location">
+                <iframe class="map--google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3551.736917901341!2d-52.619512484751894!3d-27.101589983046637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e4b5ef6657a645%3A0x52f79fb538841bef!2sAv.%20Fernando%20Machado%20-%20Centro%2C%20Chapec%C3%B3%20-%20SC!5e0!3m2!1spt-BR!2sbr!4v1615229175183!5m2!1spt-BR!2sbr"
+                    style="border:none;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+        </div>
+
+    </footer>
+
+
+</body>
