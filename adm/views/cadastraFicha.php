7b5a8e1d29a890166cd314dd9f069ebad49de5c5
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-04-24 20:54:42 -0300

    alterando ficha

--- adm/views/cadastraFicha.php
+++ adm/views/cadastraFicha.php
@@ -1,54 +1,104 @@
 <?php require_once("../includes/header.php");?>
+
 <main>
-    <h1>cadastro</h1>
+
+    <h1>Cadastro de ficha</h1>
+
     <div class="erro_cadastro">
-    <?php
-    if(isset($erros) && count($erros) !=0){
-        echo "<ul>";
-        foreach($erros as $e)
-            echo "<li>$e</li>";
-        echo "</ul>";
-    }
-    
-    $codAnimal = isset($_POST['field_codAnimal']) ? $_POST['field_codAnimal'] : "";
-    $dtNascimento = isset($_POST['field_dtNascimento']) ? $_POST['field_dtNascimento'] : "";
-    $codMae = isset($_POST['field_codMae']) ? $_POST['field_codMae'] : "";
-    $nomePai = isset($_POST['field_nomePai']) ? $_POST['field_nomePai'] : "";
-    $estadoVida = isset($_POST['field_estadoVida']) ? $_POST['field_estadoVida'] : "";
-    ?>
-    
+        <?php
+        if(isset($erros) && count($erros) !=0){
+            echo "<ul>";
+            foreach($erros as $e)
+                echo "<li>$e</li>";
+            echo "</ul>";
+        }
+        
+        $codAnimal = isset($_POST['field_codAnimal']) ? $_POST['field_codAnimal'] : "";
+        $dtNascimento = isset($_POST['field_dtNascimento']) ? $_POST['field_dtNascimento'] : "";
+        $codMae = isset($_POST['field_codMae']) ? $_POST['field_codMae'] : "";
+        $nomePai = isset($_POST['field_nomePai']) ? $_POST['field_nomePai'] : "";
+        $estadoVida = isset($_POST['field_estadoVida']) ? $_POST['field_estadoVida'] : "";
+        ?>
     </div>
 
-    <br><br>
-
     <form action="" method="post" enctype="multipart/form-data">
-        <input type="hidden" name="field_codAnimal" value="<?=$codAnimal ?>">
-        <div>
-            <label for="id_nome">Data de Nascimento: </label>
-            <input type="date" name="field_dtNascimento" size="100" maxlength="100" id="id_dtNascimento" value="<?=$dtNascimento?>">
+      <div>
+        <input type="number" placeholder="Código do animal" name="field_codAnimal" value="<?=$codAnimal ?>" required>
+        <input type="date" name="field_dtNascimento" size="100" maxlength="100" id="id_dtNascimento" value="<?=$dtNascimento?>" required>
+        <input type="number" placeholder="Código mae" name="field_codMae" size="100" maxlength="100" id="id_codMae" value="<?=$codMae?>" required>
+        <input type="text" placeholder="Nome do touro-pai" name="field_nomePai" size="50" maxlength="50" id="id_nomePai" autofocus value="<?=$nomePai?>" required>
+        <input type="number" placeholder="Estado de vida" name="field_estadoVida" size="50" maxlength="50" id="id_estadoVida" autofocus value="<?=$estadoVida?>" required>
+        <input type="file" name="field_imagem" id="id_imagem" required>
+        <div class="container-historicos">
+          <div id="button-historico-inseminacao" class="historico"><label>Adicionar histórico de inseminação</label></div>
+          <div id="button-historico-producao" class="historico"><label>Adicionar histórico de produção de leite</label></div>
+          <div id="button-historico-medicacao" class="historico"><label>Adicionar histórico de medicação</label></div>
         </div>
-        <div>
-            <label for="id_nome">Código da mãe: </label>
-            <input type="number" name="field_codMae" size="100" maxlength="100" id="id_codMae" value="<?=$codMae?>">
-        </div>
-        <div>
-            <label for="id_nome">Nome do pai: </label>
-            <input type="text" name="field_nomePai" size="50" maxlength="50" id="id_nomePai" autofocus value="<?=$nomePai?>">
-        </div>
-        <div>
-            <label for="id_nome">Estado de vida : </label>
-            <input type="number" name="field_estadoVida" size="50" maxlength="50" id="id_estadoVida" autofocus value="<?=$estadoVida?>">
-        </div>
-        <div>
-            <label for="id_nome">Imagem: </label>
-            <input type="file" name="field_imagem" id="id_imagem" required>
-        </div>
-        <br>
-        <div>
-            <input type="submit" value="Confirmar" name="cadastrar">
-            <input type="reset" value="Limpar campos">
-        </div>
+        <button class="button-form" type="submit">Salvar</button>
+      </div>
     </form>
-    </form>
-    <br><br><br><br>
+
+    <div id="modal_insem" class="modal">
+        <div class="modal-content">
+            <span id="close_insem" class="close">&times;</span>
+            <form action="adicionar-ficha.php">
+                <div>
+                    <h1>Adicionar histórico de inseminação</h1>
+                    <input type="number" placeholder="Código da vaca-mãe" required>
+                    <input type="date" required> 
+                    <input type="text" placeholder="Nome do touro inseminador" required>
+                    <input type="text" placeholder="Inseminador responsável" required>
+                    <input type="text" placeholder="Retorno">
+                    <input type="date"> <!--data de retorno da nova inseminacao-->
+                    <input type="text" placeholder="Observação">
+                    <!--previsao de parto-->
+                    <!--previsao de secagem-->
+                    <button class="button-form" type="submit">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+  <div id="modal_prod" class="modal">
+    <div class="modal-content">
+      <span id="close_prod" class="close">&times;</span>
+      <form action="adicionar-ficha.php">
+        <div>
+          <h1>Adicionar histórico de produção de leite</h1>
+          <input type="number" placeholder="Código do animal" required>
+          <input type="date" required>
+          <input type="number" step="0.01" placeholder="Produção em Litros" required>
+          <select name="periodoDia" required>
+            <option value="manha">Manhã</option>
+            <option value="tarde">Tarde</option>
+          </select>
+          <button class="button-form" type="submit">Salvar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div id="modal_medi" class="modal">
+    <div class="modal-content">
+      <span id="close_medi" class="close">&times;</span>
+      <form action="adicionar-ficha.php">
+        <div>
+          <h1>Adicionar histórico de tratamento</h1>
+          <input type="number" placeholder="Código do animal" required>
+          <input type="date" required>
+          <input type="text" placeholder="Motivos do tratamento">
+          <input type="text" placeholder="Nome do medicamento">
+          <input type="text" placeholder="Quantidade de medicamento">
+          <input type="text" placeholder="Responsável" required>
+          <input type="text" placeholder="Observação">
+          <button class="button-form" type="submit">Salvar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <?php require_once("../includes/footer.php");?>
+
+  <script src="../js/fichas.js"></script>
+    
 </main>
--- adm/views/listaFicha.php
+++ adm/views/listaFicha.php
@@ -1,10 +1,60 @@
-<?php require_once("../includes/header.php");?>
-    <main>
-        <h1><?=$titulo ?></h1>
-        <button class="button-form" onclick="window.location.href='fichaController.php?acao=adiciona'">Adicionar ficha</button>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-        <div class="lista_fichas">
-            <?php require_once("../includes/ficha.php");?>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link href="../css/style.css" rel="stylesheet">
+  <link href="../css/adicionar-ficha.css" rel="stylesheet">
+  <link href="../css/lista-fichas.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php require_once("../includes/header.php");?>
+
+<iv class="lista_fichas">
+
+    <h1><?=$titulo ?></h1>
+    <button class="button-form" onclick="window.location.href='fichaController.php?acao=adiciona'">Adicionar ficha</button>
+  
+        <?php
+        require_once "../classes/FichaDAO.php";
+        $obj = new FichaDAO();
+        $lista = $obj->listar(); 
+        if(count($lista) == 0){
+            echo "Nenhuma ficha encontrada.";
+        }else{
+            foreach ($lista as $ficha){
+        ?>     
+   
+        <div class="ficha">
+            <div class="label">
+                <div class="ficha_img">
+                    <img src="../img/<?=$ficha->getNomeImagem()?>" alt="imagem_animal">
+                </div>
+                <div class="ficha_descricao">
+                    <strong>Código: <?=$ficha->getCodAnimal()?> </strong>
+                    <strong>Código da mãe: <?=$ficha->getCodMae() ?> </strong>
+                </div>
+                <div>
+                    <button class="button-edit" onclick="window.location.href='fichaController.php?acao=altera&codAnimal=<?=$ficha->getCodAnimal() ?>'"><i class="fa fa-edit fa-1x"></i></button>
+                    <button class="button-delete" onclick="window.location.href='fichaController.php?acao=exclui&codAnimal=<?=$ficha->getCodAnimal() ?>'"><i class="fa fa-trash-alt fa-1x"></i></button>
+                </div>
+            </div>
         </div>
-        
-    </main>
+
+    <?php
+        }
+      }
+    ?>
+    
+  </div>
+
+  <?php require_once("../includes/footer.php");?>
+  
+  <script src="/js/lista-fichas.js"></script>
+
+</body>    
