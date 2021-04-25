51778d43aa0e2d62d90164906b79cedd53273f42
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-04-25 17:29:26 -0300

    retirando comentarios

--- adm/fichaController.php
+++ adm/fichaController.php
@@ -1,7 +1,6 @@
 <?php
 include_once "../classes/FichaDAO.php";
 if(!isset($_GET['acao'])){
-    // pg inicial de adm. de sabores; carrega lista de registros
     $titulo = "Lista de Fichas";
     $obj = new FichaDAO();
     $lista = $obj->listar();
@@ -12,11 +11,10 @@ else {
 
         case 'adiciona':
             $titulo = "Adiciona Ficha";
-            // logica para cadastro
-            if(!isset($_POST['adiciona'])){ // dados ainda nao submetidos
+            if(!isset($_POST['adiciona'])){ 
                 include "views/cadastraFicha.php";              
             }
-            else{ // dados submetidos; trata a inserção
+            else{
                 $novo = new Ficha();
                 $obj->setCodAnimal($_POST['field_codAnimal']);
                 $obj->setDtNascimento($_POST['field_dtNascimento']);
@@ -25,36 +23,32 @@ else {
                 $obj->setEstadoVida($_POST['field_estadoVida']);
                 $obj->setNomeImagem($_FILES['field_imagem']['name']);
                 $erros = $novo->validate();
-                if(count($erros) != 0){ // algum campo em branco
+                if(count($erros) != 0){ 
                     include "views/cadastraFicha.php";                       
                 }
-                else{ // campos todos preenchidos
-                    //upload
+                else{ 
                     $destino = "../img/".$_FILES['field_imagem']['name']; 
                     if(move_uploaded_file($_FILES['field_imagem']['tmp_name'], $destino)){
-                        //inserção
                         $bd = new FichaDAO();
                         if($bd->inserir($novo))
-                            header("Location: fichaController.php"); // redireciona
+                            header("Location: fichaController.php"); 
                     }
                     else{
-                        // erro no upload
                         $erros[] = "Erro no upload";
                         include "views/cadastraFicha.php";                      
                     }
                 }
             }
             break;
-
         
         case 'altera':
             $titulo = "Alteração de Ficha";
-            if(!isset($_POST['altera'])){ // dados ainda nao submetidos; carrega os dados atuais
+            if(!isset($_POST['altera'])){ 
                 $obj = new FichaDAO();
                 $ficha = $obj->buscar($_GET['codAnimal']);
                 include "views/alteraFicha.php";
             }
-            else{ // dados submetidos; efetua a alteração
+            else{ 
                 $obj = new Ficha();
                 $obj->setDtNascimento($_POST['field_dtNascimento']);
                 $obj->setCodMae($_POST['field_codMae']);
@@ -63,20 +57,17 @@ else {
                 $obj->setNomeImagem($_FILES['field_imagem']['name']);
                 $obj->setCodAnimal($_POST['field_codAnimal']);
                 $erros = $obj->validate();
-                if(count($erros) != 0){ // algum campo em branco
+                if(count($erros) != 0){
                     include "views/alteraFicha.php";                      
                 }
-                else{ // campos todos preenchidos
-                    //upload
+                else{ 
                     $destino = "../img/".$_FILES['field_imagem']['name']; 
                     if(move_uploaded_file($_FILES['field_imagem']['tmp_name'], $destino)){
-                        //inserção
                         $bd = new FichaDAO();
                         if($bd->alterar($obj))
-                            header("Location: fichaController.php"); // redireciona
+                            header("Location: fichaController.php"); 
                     }
                     else{
-                        // erro no upload
                         $erros[] = "Erro no upload";
                         include "views/cadastraFicha.php";                        
                     }
@@ -84,19 +75,18 @@ else {
             }
             break;
 
-        
         case 'exclui':
             $bd = new FichaDAO();
             if($bd->excluir($_GET['codAnimal']))
-                header("Location: fichaController.php"); // redireciona
+                header("Location: fichaController.php"); 
 
             break;
         
         default:
             echo "Ação não permitida";  
                       
-    }// fim switch
-} // fim else
+    }
+} 
 
 ?>
 
--- includes/footer.php
+++ includes/footer.php
@@ -2,14 +2,13 @@
 <html lang="pt-br">
 
 <head>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="shortcut icon" href="/img/mst.png">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" rel="stylesheet" />
     <link href="../css/footer.css" rel="stylesheet">
-    <title>COOPTAR</title>
+
 </head>
 
 <body>
