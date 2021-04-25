3fc73a3f40cb8c7155cb57ebc8e041bc9f6308f3
Pedro Manfio Lill <pedro.manfiolill@gmail.com>
2021-04-25 18:26:10 -0300

    inseminacaoes

--- controller/views/cadastraFicha.php
+++ controller/views/cadastraFicha.php
@@ -5,19 +5,36 @@
     <h1>Cadastro de ficha</h1>
 
     <div class="erro_cadastro">
-        <?php
-        if(isset($erros) && count($erros) !=0){
-            echo "<ul>";
-            foreach($erros as $e)
-                echo "<li>$e</li>";
-            echo "</ul>";
-        }
-        
-        $codAnimal = isset($_POST['field_codAnimal']) ? $_POST['field_codAnimal'] : "";
-        $dtNascimento = isset($_POST['field_dtNascimento']) ? $_POST['field_dtNascimento'] : "";
-        $codMae = isset($_POST['field_codMae']) ? $_POST['field_codMae'] : "";
-        $nomePai = isset($_POST['field_nomePai']) ? $_POST['field_nomePai'] : "";
-        $estadoVida = isset($_POST['field_estadoVida']) ? $_POST['field_estadoVida'] : "";
+      <?php
+      if(isset($erros) && count($erros) !=0){
+          echo "<ul>";
+          foreach($erros as $e)
+              echo "<li>$e</li>";
+          echo "</ul>";
+      }
+      //fichas
+      $codAnimal = isset($_POST['field_codAnimal']) ? $_POST['field_codAnimal'] : "";
+      $dtNascimento = isset($_POST['field_dtNascimento']) ? $_POST['field_dtNascimento'] : "";
+      $codMae = isset($_POST['field_codMae']) ? $_POST['field_codMae'] : "";
+      $nomePai = isset($_POST['field_nomePai']) ? $_POST['field_nomePai'] : "";
+      $estadoVida = isset($_POST['field_estadoVida']) ? $_POST['field_estadoVida'] : "";
+
+      //inseminacao
+      //$codAnimal = isset($_POST['field_codAnimal']) ? $_POST['field_codAnimal'] : "";
+      $codInseminacao = isset($_POST['field_codInseminacao']) ? $_POST['field_codInseminacao'] : "";
+      $dtInseminacao = isset($_POST['field_dtInseminacao']) ? $_POST['field_dtInseminacao'] : "";
+      $touroInseminador = isset($_POST['field_touroInseminador']) ? $_POST['field_touroInseminador'] : "";
+      $inseminadorResponsavel = isset($_POST['field_inseminadorResponsavel']) ? $_POST['field_inseminadorResponsavel'] : "";
+      $retorno = isset($_POST['field_retorno']) ? $_POST['field_retorno'] : "";
+      $previsaoSecagem = isset($_POST['field_previsaoSecagem']) ? $_POST['field_previsaoSecagem'] : "";
+      $previsaoParto = isset($_POST['field_previsaoParto']) ? $_POST['field_previsaoParto'] : "";
+      $novaDtInseminacao = isset($_POST['field_novaDtInseminacao']) ? $_POST['field_novaDtInseminacao'] : "";
+      $novaPrevisaoSecagem = isset($_POST['field_novaPrevisaoSecagem']) ? $_POST['field_novaPrevisaoSecagem'] : "";
+      $novaPrevisaoParto = isset($_POST['field_novaPrevisaoParto']) ? $_POST['field_novaPrevisaoParto'] : "";
+
+      //produção
+
+
         ?>
     </div>
 
@@ -39,24 +56,24 @@
     </form>
 
     <div id="modal_insem" class="modal">
-        <div class="modal-content">
-            <span id="close_insem" class="close">&times;</span>
-            <form action="adicionar-ficha.php">
-                <div>
-                    <h1>Adicionar histórico de inseminação</h1>
-                    <input type="number" placeholder="Código da vaca-mãe" required>
-                    <input type="date" required> 
-                    <input type="text" placeholder="Nome do touro inseminador" required>
-                    <input type="text" placeholder="Inseminador responsável" required>
-                    <input type="text" placeholder="Retorno">
-                    <input type="date"> <!--data de retorno da nova inseminacao-->
-                    <input type="text" placeholder="Observação">
-                    <!--previsao de parto-->
-                    <!--previsao de secagem-->
-                    <button class="button-form" type="submit">Salvar</button>
-                </div>
-            </form>
-        </div>
+      <div class="modal-content">
+        <span id="close_insem" class="close">&times;</span>
+        <form action="adicionar-ficha.php">
+          <div>
+            <h1>Adicionar histórico de inseminação</h1>
+            <input type="number" placeholder="Código da vaca"  name="field_dtNascimento" size="100" maxlength="100" id="id_dtNascimento" value="<?=$dtNascimento?>" required>
+            <input type="date" required> 
+            <input type="text" placeholder="Nome do touro inseminador" required>
+            <input type="text" placeholder="Inseminador responsável" required>
+            <input type="text" placeholder="Retorno">
+            <input type="date"> <!--data de retorno da nova inseminacao-->
+            <input type="text" placeholder="Observação">
+            <!--previsao de parto-->
+            <!--previsao de secagem-->
+            <button class="button-form" type="submit">Salvar</button>
+          </div>
+        </form>
+      </div>
     </div>
 
   <div id="modal_prod" class="modal">
