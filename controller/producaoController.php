31318787050a4c636c093b26199526ab0a5ca240
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-04-25 19:15:51 -0300

    adicionando controllers

--- controller/producaoController.php
+++ controller/producaoController.php
@@ -1,8 +1,7 @@
 <?php
-include_once "../controller/classes/inseminacaoDAO.php";
+include_once "../controller/classes/ProducaoDAO.php";
 if(!isset($_GET['acao'])){
-    $titulo = "Inseminações";
-    $obj = new inseminacaoDAO();
+    $obj = new ProducaoDAO();
     $lista = $obj->listar();
     include "views/cadastraFicha.php";
 }
@@ -10,76 +9,48 @@ else {
 	switch($_GET['acao']){
 
         case 'adiciona':
-            $titulo = "Adiciona Inseminação";
             if(!isset($_POST['adiciona'])){ 
                 include "views/cadastraFicha.php";              
             }
             else{
-                $novo = new Ficha();
+                $novo = new Producao();
                 $obj->setCodAnimal($_POST['field_codAnimal']);
                 $obj->setDtNascimento($_POST['field_dtNascimento']);
                 $obj->setCodMae($_POST['field_codMae']);
                 $obj->setNomePai($_POST['field_nomePai']);
                 $obj->setEstadoVida($_POST['field_estadoVida']);
-                $obj->setNomeImagem($_FILES['field_imagem']['name']);
                 $erros = $novo->validate();
                 if(count($erros) != 0){ 
                     include "views/cadastraFicha.php";                       
-                }
-                else{ 
-                    $destino = "../img/".$_FILES['field_imagem']['name']; 
-                    if(move_uploaded_file($_FILES['field_imagem']['tmp_name'], $destino)){
-                        $bd = new FichaDAO();
-                        if($bd->inserir($novo))
-                            header("Location: fichaController.php"); 
-                    }
-                    else{
-                        $erros[] = "Erro no upload";
-                        include "views/cadastraFicha.php";                      
-                    }
                 }
             }
             break;
         
         case 'altera':
-            $titulo = "Alteração de Ficha";
+            $titulo = "Alteração de producao de leite";
             if(!isset($_POST['altera'])){ 
                 $obj = new FichaDAO();
-                $ficha = $obj->buscar($_GET['codAnimal']);
+                $ficha = $obj->buscar($_GET['codProducao']);
                 include "views/alteraFicha.php";
             }
             else{ 
-                $obj = new Ficha();
+                $obj = new Producao();
                 $obj->setDtNascimento($_POST['field_dtNascimento']);
                 $obj->setCodMae($_POST['field_codMae']);
                 $obj->setNomePai($_POST['field_nomePai']);
                 $obj->setEstadoVida($_POST['field_estadoVida']);
-                $obj->setNomeImagem($_FILES['field_imagem']['name']);
                 $obj->setCodAnimal($_POST['field_codAnimal']);
                 $erros = $obj->validate();
                 if(count($erros) != 0){
                     include "views/alteraFicha.php";                      
                 }
-                else{ 
-                    $destino = "../img/".$_FILES['field_imagem']['name']; 
-                    if(move_uploaded_file($_FILES['field_imagem']['tmp_name'], $destino)){
-                        $bd = new FichaDAO();
-                        if($bd->alterar($obj))
-                            header("Location: fichaController.php"); 
-                    }
-                    else{
-                        $erros[] = "Erro no upload";
-                        include "views/cadastraFicha.php";                        
-                    }
-                }
             }
             break;
 
         case 'exclui':
-            $bd = new FichaDAO();
-            if($bd->excluir($_GET['codAnimal']))
-                header("Location: fichaController.php"); 
-
+            $bd = new ProducaoDAO();
+            if($bd->excluir($_GET['codProducao']))
+                header("Location: producaoController.php"); 
             break;
         
         default:
--- controller/tratamentoController.php
+++ controller/tratamentoController.php
@@ -1,8 +1,8 @@
 <?php
-include_once "../controller/classes/inseminacaoDAO.php";
+include_once "../controller/classes/TratamentoDAO.phpDAO.php";
 if(!isset($_GET['acao'])){
-    $titulo = "Inseminações";
-    $obj = new inseminacaoDAO();
+    $titulo = "Tratamentos";
+    $obj = new tratamentoDAO();
     $lista = $obj->listar();
     include "views/cadastraFicha.php";
 }
@@ -10,42 +10,29 @@ else {
 	switch($_GET['acao']){
 
         case 'adiciona':
-            $titulo = "Adiciona Inseminação";
+            $titulo = "Adiciona tratamento";
             if(!isset($_POST['adiciona'])){ 
                 include "views/cadastraFicha.php";              
             }
             else{
-                $novo = new Ficha();
+                $novo = new Tratamento();
                 $obj->setCodAnimal($_POST['field_codAnimal']);
                 $obj->setDtNascimento($_POST['field_dtNascimento']);
                 $obj->setCodMae($_POST['field_codMae']);
                 $obj->setNomePai($_POST['field_nomePai']);
                 $obj->setEstadoVida($_POST['field_estadoVida']);
-                $obj->setNomeImagem($_FILES['field_imagem']['name']);
                 $erros = $novo->validate();
                 if(count($erros) != 0){ 
                     include "views/cadastraFicha.php";                       
-                }
-                else{ 
-                    $destino = "../img/".$_FILES['field_imagem']['name']; 
-                    if(move_uploaded_file($_FILES['field_imagem']['tmp_name'], $destino)){
-                        $bd = new FichaDAO();
-                        if($bd->inserir($novo))
-                            header("Location: fichaController.php"); 
-                    }
-                    else{
-                        $erros[] = "Erro no upload";
-                        include "views/cadastraFicha.php";                      
-                    }
                 }
             }
             break;
         
         case 'altera':
-            $titulo = "Alteração de Ficha";
+            $titulo = "Alteração de tratamento";
             if(!isset($_POST['altera'])){ 
                 $obj = new FichaDAO();
-                $ficha = $obj->buscar($_GET['codAnimal']);
+                $tratamento = $obj->buscar($_GET['codAnimal']);
                 include "views/alteraFicha.php";
             }
             else{ 
@@ -54,32 +41,18 @@ else {
                 $obj->setCodMae($_POST['field_codMae']);
                 $obj->setNomePai($_POST['field_nomePai']);
                 $obj->setEstadoVida($_POST['field_estadoVida']);
-                $obj->setNomeImagem($_FILES['field_imagem']['name']);
                 $obj->setCodAnimal($_POST['field_codAnimal']);
                 $erros = $obj->validate();
                 if(count($erros) != 0){
                     include "views/alteraFicha.php";                      
                 }
-                else{ 
-                    $destino = "../img/".$_FILES['field_imagem']['name']; 
-                    if(move_uploaded_file($_FILES['field_imagem']['tmp_name'], $destino)){
-                        $bd = new FichaDAO();
-                        if($bd->alterar($obj))
-                            header("Location: fichaController.php"); 
-                    }
-                    else{
-                        $erros[] = "Erro no upload";
-                        include "views/cadastraFicha.php";                        
-                    }
-                }
             }
             break;
 
         case 'exclui':
-            $bd = new FichaDAO();
+            $bd = new TratamentoDAO();
             if($bd->excluir($_GET['codAnimal']))
-                header("Location: fichaController.php"); 
-
+                header("Location: tratamentoController.php"); 
             break;
         
         default:
