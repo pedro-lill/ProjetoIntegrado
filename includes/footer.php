eb6a94dcc1a5a49669ebb9cdf5df177c1492639d
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-04-24 21:40:37 -0300

    acertando maps footer

--- includes/footer.php
+++ includes/footer.php
@@ -14,7 +14,7 @@
 
 <body>
 
-<footer>
+    <footer>
         <div class="container__footer">
             <div class="container__text">
                 <span>COOPTAR - MST</span>
@@ -22,12 +22,9 @@
                 <span>Pedro Lill e Fabrício Romani</span>
             </div>
             <div class="container__location">
-                <iframe class="map--google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3551.736917901341!2d-52.619512484751894!3d-27.101589983046637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e4b5ef6657a645%3A0x52f79fb538841bef!2sAv.%20Fernando%20Machado%20-%20Centro%2C%20Chapec%C3%B3%20-%20SC!5e0!3m2!1spt-BR!2sbr!4v1615229175183!5m2!1spt-BR!2sbr"
-                    style="border:none;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3522.99024068874!2d-52.79040768473425!3d-27.994194982674035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fcd41d897baa35%3A0x32d693b15a02d822!2sCooptar%20-%20Cooperativa%20de%20Produ%C3%A7%C3%A3o%20Agropecu%C3%A1ria%20Cascata!5e0!3m2!1spt-BR!2sbr!4v1619311164437!5m2!1spt-BR!2sbr" style="border:none;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
             </div>
         </div>
-
     </footer>
-
 
 </body>
