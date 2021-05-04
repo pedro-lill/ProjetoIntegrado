a77ed919caf9b05b2690054c0fb398fcc2f90718
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-05-04 12:10:51 -0300

    ajeitando outros placeholder

--- controller/views/cadastraProducao.php
+++ controller/views/cadastraProducao.php
@@ -21,13 +21,25 @@
       <form action="" method="post" enctype="multipart/form-data">
         <div>
           <h1>Cadastro de produção</h1>
-          <input type="number" placeholder="Código do animal" name="field_codAnimal" autofocus value="<?=$codAnimal ?>" required>
-          <input type="datetime-local" name="field_dtColeta" maxlength="100" id="id_dtColeta" value="<?=$dtColeta?>" required>
-          <input type="number" step="0.01" placeholder="Produção em Litros" name="field_litros" autofocus value="<?=$litros?>" required>
-          <select required>
-            <option  name="field_periodoDia" value="manha" autofocus value="<?=$periodoDia?>">Manhã</option>
-            <option  name="field_periodoDia" value="tarde" autofocus value="<?=$periodoDia?>">Tarde</option>
-          </select>
+          <div>
+            <label class="label-cadastra" for="id_codAnimal">Código do animal: </label>
+            <input type="number" name="field_codAnimal" id="id_codAnimal" autofocus value="<?=$codAnimal ?>" required>
+          </div>
+          <div>
+            <label class="label-cadastra" for="id_dtColeta">Data da coleta: </label>
+            <input type="datetime-local" name="field_dtColeta" maxlength="100" id="id_dtColeta" value="<?=$dtColeta?>" required>
+          </div>
+          <div>
+            <label class="label-cadastra" for="id_litros">Produção em litros: </label>
+            <input type="number" step="0.01" name="field_litros" id="id_litros" autofocus value="<?=$litros?>" required>
+          </div>
+          <div>
+            <label class="label-cadastra" for="id_periodoDia">Periodo do dia: </label>
+            <select id="id_periodoDia" required >
+              <option  name="field_periodoDia" value="manha" autofocus value="<?=$periodoDia?>">Manhã</option>
+              <option  name="field_periodoDia" value="tarde" autofocus value="<?=$periodoDia?>">Tarde</option>
+            </select>
+          </div>
             <button name="adiciona" class="button-form" type="submit">Salvar</button>
         </div>
       </form>
