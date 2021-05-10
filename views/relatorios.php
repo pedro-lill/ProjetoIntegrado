90a30afb44b132ffd1a8c9b700a435a909aa4495
Pedro Manfio Lill <pedro.manfiolill@gmail.com>
2021-05-10 13:46:47 -0300

    relatorio

--- views/relatorios.php
+++ views/relatorios.php
@@ -16,21 +16,24 @@
 <h1>RELATORIOS</h1>
 
 <div>
-    <select id="id_mes" required >
-        <option name="field_mes" value="jan" value="<?=$mes?>">Janeiro</option>
-        <option name="field_mes" value="fev" value="<?=$mes?>">Fevereiro</option>
-        <option name="field_mes" value="mar" value="<?=$mes?>">Março</option>
-        <option name="field_mes" value="mai" value="<?=$mes?>">Maio</option>
-        <option name="field_mes" value="abr" value="<?=$mes?>">Abril</option>
-        <option name="field_mes" value="jun" value="<?=$mes?>">Junho</option>
-        <option name="field_mes" value="jul" value="<?=$mes?>">Julho</option>
-        <option name="field_mes" value="ago" value="<?=$mes?>">Agosto</option>
-        <option name="field_mes" value="set" value="<?=$mes?>">Setembro</option>
-        <option name="field_mes" value="out" value="<?=$mes?>">Outubro</option>
-        <option name="field_mes" value="nov" value="<?=$mes?>">Novembro</option>
-        <option name="field_mes" value="dez" value="<?=$mes?>">Dezembro</option>
-    </select>
-    <a class="relatorio-a" href="../controller/relatorioController.php?acao=adiciona&filtroMes=<?$_POST['field_mes'] ?>'"><i class="fas fa-search"></i></a>
+    <form action="submit">
+        <select id="id_mes" required >
+            <option name="field_mes" value="jan" value="<?=$mes?>">Janeiro</option>
+            <option name="field_mes" value="fev" value="<?=$mes?>">Fevereiro</option>
+            <option name="field_mes" value="mar" value="<?=$mes?>">Março</option>
+            <option name="field_mes" value="mai" value="<?=$mes?>">Maio</option>
+            <option name="field_mes" value="abr" value="<?=$mes?>">Abril</option>
+            <option name="field_mes" value="jun" value="<?=$mes?>">Junho</option>
+            <option name="field_mes" value="jul" value="<?=$mes?>">Julho</option>
+            <option name="field_mes" value="ago" value="<?=$mes?>">Agosto</option>
+            <option name="field_mes" value="set" value="<?=$mes?>">Setembro</option>
+            <option name="field_mes" value="out" value="<?=$mes?>">Outubro</option>
+            <option name="field_mes" value="nov" value="<?=$mes?>">Novembro</option>
+            <option name="field_mes" value="dez" value="<?=$mes?>">Dezembro</option>
+        </select>
+    </form>
+    
+    <a class="relatorio-a" href="../controller/relatorioController.php?acao=adiciona&filtroMes=<?=$_POST['field_mes'] ?>'"><i class="fas fa-search"></i></a>
 </div>
 
 <?php require_once("../includes/footer.php");?>
