4b53592df2b8193f25afbfb367041b3d87a485f3
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-05-16 19:32:32 -0300

    mudando icones

--- views/configuracoes.php
+++ views/configuracoes.php
@@ -3,9 +3,9 @@
 <h3>Configurações</h3>
 
 <ul class="lista-configuracoes">
-    <li><a href="../controller/usuarioController.php?acao=adiciona"><i class="fas fa-user"></i>     Cadastrar novo usuario</a></li>
+    <li><a href="../controller/usuarioController.php?acao=adiciona"><i class="fas fa-user-plus"></i>     Cadastrar novo usuario</a></li>
     <li><a href="../controller/usuarioController.php?acao=altera"><i class="fas fa-user-cog"></i>     Editar minha conta</a></li>
-    <li><a href="../index.php"><i class="fas fa-user-cog"></i>     Sair</a></li>
+    <li><a href="../index.php"><i class="fas fa-sign-out-alt"></i>     Sair</a></li>
 
 </ul>
 
