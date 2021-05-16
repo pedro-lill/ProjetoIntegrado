62e1fcd26d45eb6e08795171cdc2f0b8feaa0b64
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-05-16 11:22:41 -0300

    graficos

--- js/graficos.js
+++ js/graficos.js
@@ -2,25 +2,37 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(183, 102, 255, 0.2)',
+                'rgba(253, 102, 255, 0.2)',
+                'rgba(143, 102, 255, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
+                'rgba(54, 162, 235,  1)',
+                'rgba(255, 206, 86,  1)',
+                'rgba(75, 192, 192,  1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 159, 64,  1)',
+                'rgba(183, 102, 255, 1)',
+                'rgba(253, 102, 255, 1)',
+                'rgba(143, 102, 255, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(54, 162, 235,  1)',
             ],
             borderWidth: 1.5
         }]
--- views/graficos.php
+++ views/graficos.php
@@ -2,22 +2,25 @@
 
 <h3>GRÁFICOS</h3>
 
-<h3>Possíveis partos nos meses de 2021</h3>
 
-<div class="grafico-prox-partos">
-    <canvas id="myChart"></canvas>
-</div>
+<div class="graficos-container">
+    
+    <div class="grafico-prox-partos">
+    <h3>Possíveis partos nos meses de 2021</h3>
 
+        <canvas id="myChart"></canvas>
+    </div>
 
-<h3>Produção de leite / 2020</h3>
-<div class="grafico-prod-leite">
-    <canvas id="myChartProdLeite"></canvas>
+    <div class="grafico-prod-leite">
+    <h3>Produção de leite / 2020</h3>
+
+        <canvas id="myChartProdLeite"></canvas>
+    </div>
+
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
 <script src="../js/graficos.js"></script>    
 
-
 <?php require_once("../includes/footer.php");?>
-
