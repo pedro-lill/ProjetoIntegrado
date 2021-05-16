f7d168bff2ff312d1ed4fbe193f0ddbbff95f1ec
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-05-15 22:16:45 -0300

    graficos

--- views/graficos.php
+++ views/graficos.php
@@ -1,9 +1,15 @@
 <?php require_once("../includes/header.php");?>
 
 <h3>GRÁFICOS</h3>
-<?php
-$consulta="select dtInseminacao from inseminacao"
 
-?>
+<h3>Possíveis partos nos meses de 2021</h3>
+
+<canvas id="myChart"></canvas>
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+<script src="../js/graficos.js"></script>    
+
 
 <?php require_once("../includes/footer.php");?>
+
