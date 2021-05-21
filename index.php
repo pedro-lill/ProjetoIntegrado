bfc030e02015579a8cffb70f4a01604e6b89b240
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-05-21 13:47:20 -0300

    index

--- index.php
+++ index.php
@@ -24,22 +24,49 @@
   </ul>
 </header>
 
-<br>
-<br>
-<br>
-<br>
-<br>
+<div>
+        <img class="banner" title="Foto da pizzaria" src="img/banner.png" />
+    </div>
 
-<div class="opcoes-menu-index">
-  <a href="login.php" name="leiteiro" id="leiteiro"></a>
-  <label for="leiteiro"> Leiteiro</label>
-  <label> Leiteiro</label>
-  <label> Leiteiro</label>
-  <label> Leiteiro</label>
-</div>
+    <div class="textoPizzaria" id="TextoSobreJS">
+        <h2>Pizzaria Hermanos</h2>
+        <p>Acreditamos que cada pedaço de pizza representa em seu próprio ser uma experiência completa.</p>
+    </div>
+
+    <div class="fotosPizzaria" id="FotosJS">
+        <h2>Galeria de fotos</h2>
+
+        <section class="galeria">
+            <a href="views/quemSomos.php"><img src="img/imagemPizzaria1.jfif" alt="imagem-promocao" onclick="" /></a>
+            <a href="views/quemSomos.php"><img src="img/imagemPizzaria2.jfif" alt="imagem-promocao" onclick="" /></a>
+            <a href="views/quemSomos.php"><img src="img/imagemPizzaria3.jfif" alt="imagem-promocao" onclick="" /></a>
+            <a href="views/quemSomos.php"><img src="img/imagemPizzaria4.jpg" alt="imagem-promocao" onclick="" /></a>
+            <a href="views/quemSomos.php"><img src="img/imagemPizzaria5.jfif" alt="imagem-promocao" onclick="" /></a>
+            <a href="views/quemSomos.php"><img src="img/imagemPizzaria6.jpg" alt="imagem-promocao" onclick="" /></a>
+        </section>
+    </div>
+
+    <div class="contato" id="ContatoJS">
+        <form class="form">
+            <h2>Faça seu pedido!</h2>
+            <div class="div-metade">
+            <input placeholder="Nome" type="text" required>
+            </div>
+            <div class="div-metade">
+            <input placeholder="Telefone" type="number" required>
+            </div>
+            <div class="div-inteira">
+            <input placeholder="E-mail" type="email" required>
+            </div>
+            <div class="div-inteira">
+            <input type="text" placeholder="Digite sua mensagem aqui" maxlength="600" required></input>
+            </div>
+            <button class="field-enviar" type="submit">Enviar</button>
+        </form>
+    </div>
 
 <script src="js/header.js"></script>
 
-
+<?php require_once("includes/footer.php");?>
 </body>
 </html>
