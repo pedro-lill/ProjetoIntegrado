2be9412172d32e3eb6744e79b8d3057825f03398
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-05-21 13:52:15 -0300

    index

--- index.php
+++ index.php
@@ -67,6 +67,45 @@
 
 <script src="js/header.js"></script>
 
-<?php require_once("includes/footer.php");?>
+<footer class="main__footer" id="FooterJS">
+
+<div class="container__footer">
+    <div class="container__icons">
+        <ul>
+            <li>
+                <a href="https://www.instagram.com/semfronteirasjr/"><img src="/img/instagram(1).svg" height="45" width="45"></a>
+            </li>
+            <li>
+                <a href="https://br.linkedin.com/company/semfronteirasconsultoriajr"><img src="img/linkedin.svg" height="45" width="45"></a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/SemFronteirasConsultoriaJr"><img src="img/facebook.svg" width="45" height="45"></a>
+            </li>
+            <li>
+                <a href="mailto:ej.semfronteiras@gmail.com"><img src="img/o-email.svg" width="45" height="45"></a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="container__text">
+        <span>Universidade Federal da Fronteira Sul</span>
+        <span>Sala 222 - Bloco B</span>
+        <span>festec@fronteiratec.com</span>
+        <span>Chapecó - SC</span>
+        <span>(54) 999223311</span>
+        <span>CNPJ 22333991100</span>
+        <span>CEP 11993-1</span>
+    </div>
+
+    <div class="container__location">
+        <iframe class="map--google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d113646.78846818066!2d-52.732016345498185!3d-27.11024528476605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94fb4b2bbbd96a4b%3A0x5871f4d6809cb67e!2sUniversidade%20Federal%20da%20Fronteira%20Sul%2C%20Campus%20Chapec%C3%B3!5e0!3m2!1spt-BR!2sbr!4v1597082565539!5m2!1spt-BR!2sbr"
+            style="border:none;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+</div>
+
+<div class="container__copy">
+    <h5>&copy Desenvolvido por FronteiraTec</h5>
+</div>
+</footer>
 </body>
 </html>
