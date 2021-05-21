49ea5c17521d4d1c6127eccf83f1fa69e30f684a
fabricioromanii <fabricioz.1.2@hotmail.com>
2021-05-21 08:02:32 -0300

    relatorio

--- views/relatorios.php
+++ views/relatorios.php
@@ -17,41 +17,41 @@
 
     <div class="div-filtros-relatorio-tipo">
         <label class="" for="id_tipo">Tipo de relatório</label>
-        <select class="select-relatorio" id="id_tipo"  name="filtroTipo" onchange="fazerRelatorio()" required >
-            <option value="prod" value="<?=$tipo?>" <?=($_GET['acao']  == 'prod')?'selected':''?>>Produção</option>
-            <option value="ins" value="<?=$tipo?>" <?=($_GET['acao']  == 'ins')?'selected':''?>>Inseminação</option>
-            <option value="prevSec" value="<?=$tipo?>" <?=($_GET['acao']  == 'prevSec')?'selected':''?>>Previsão de secagem</option>
-            <option value="prevPa" value="<?=$tipo?>" <?=($_GET['acao']  == 'prevPa')?'selected':''?>>Previsão de parto</option>
+        <select class="select-relatorio" id="id_tipo" value="tipo"  name="filtroTipo" onchange="fazerRelatorio()" required >
+            <option value="prod"    <?=($_GET['acao']  == 'prod')?'selected':''?>>Produção</option>
+            <option value="ins"     <?=($_GET['acao']  == 'ins')?'selected':''?>>Inseminação</option>
+            <option value="prevSec" <?=($_GET['acao']  == 'prevSec')?'selected':''?>>Previsão de secagem</option>
+            <option value="prevPa"  <?=($_GET['acao']  == 'prevPa')?'selected':''?>>Previsão de parto</option>
         </select>
     </div>
 
     <div  class="div-filtros-relatorio-mes">
         <label class="" for="id_ano">Ano do relatório</label>
-        <select  class="select-relatorio" id="id_ano"  name="filtroAno" onchange="fazerRelatorio()" required >
-            <option value="2018" value="<?=$ano?>" <?=($_GET['filtroAno']  == '2018')?'selected':''?>>2018</option>
-            <option value="2019" value="<?=$ano?>" <?=($_GET['filtroAno']  == '2019')?'selected':''?>>2019</option>
-            <option value="2020" value="<?=$ano?>" <?=($_GET['filtroAno']  == '2020')?'selected':''?>>2020</option>
-            <option value="2021" value="<?=$ano?>" <?=($_GET['filtroAno']  == '2021')?'selected':''?>>2021</option>
-            <option value="2022" value="<?=$ano?>" <?=($_GET['filtroAno']  == '2022')?'selected':''?>>2022</option>
-            <option value="2023" value="<?=$ano?>" <?=($_GET['filtroAno']  == '2023')?'selected':''?>>2023</option>
+        <select  class="select-relatorio" id="id_ano" value="ano" name="filtroAno" onchange="fazerRelatorio()" required >
+            <option value="2018" <?=($_GET['filtroAno']  == '2018')?'selected':''?>>2018</option>
+            <option value="2019" <?=($_GET['filtroAno']  == '2019')?'selected':''?>>2019</option>
+            <option value="2020" <?=($_GET['filtroAno']  == '2020')?'selected':''?>>2020</option>
+            <option value="2021" <?=($_GET['filtroAno']  == '2021')?'selected':''?>>2021</option>
+            <option value="2022" <?=($_GET['filtroAno']  == '2022')?'selected':''?>>2022</option>
+            <option value="2023" <?=($_GET['filtroAno']  == '2023')?'selected':''?>>2023</option>
         </select>
     </div>
 
     <div  class="div-filtros-relatorio-mes">
         <label class="" for="id_mes">Mês do relatório</label>
-        <select  class="select-relatorio" id="id_mes"  name="filtroMes" onchange="fazerRelatorio()" required >
-            <option value="01" value="<?=$mes?>" <?=($_GET['filtroMes']  == '01')?'selected':''?>>Janeiro</option>
-            <option value="02" value="<?=$mes?>" <?=($_GET['filtroMes']  == '02')?'selected':''?>>Fevereiro</option>
-            <option value="03" value="<?=$mes?>" <?=($_GET['filtroMes']  == '03')?'selected':''?>>Março</option>
-            <option value="04" value="<?=$mes?>" <?=($_GET['filtroMes']  == '04')?'selected':''?>>Abril</option>
-            <option value="05" value="<?=$mes?>" <?=($_GET['filtroMes']  == '05')?'selected':''?>>Maio</option>
-            <option value="06" value="<?=$mes?>" <?=($_GET['filtroMes']  == '06')?'selected':''?>>Junho</option>
-            <option value="07" value="<?=$mes?>" <?=($_GET['filtroMes']  == '07')?'selected':''?>>Julho</option>
-            <option value="08" value="<?=$mes?>" <?=($_GET['filtroMes']  == '08')?'selected':''?>>Agosto</option>
-            <option value="09" value="<?=$mes?>" <?=($_GET['filtroMes']  == '09')?'selected':''?>>Setembro</option>
-            <option value="10" value="<?=$mes?>" <?=($_GET['filtroMes']  == '10')?'selected':''?>>Outubro</option>
-            <option value="11" value="<?=$mes?>" <?=($_GET['filtroMes']  == '11')?'selected':''?>>Novembro</option>
-            <option value="12" value="<?=$mes?>" <?=($_GET['filtroMes']  == '12')?'selected':''?>>Dezembro</option>
+        <select  class="select-relatorio" id="id_mes" value="mes" name="filtroMes" onchange="fazerRelatorio()" required >
+            <option value="01" <?=($_GET['filtroMes']  == '01')?'selected':''?>>Janeiro</option>
+            <option value="02" <?=($_GET['filtroMes']  == '02')?'selected':''?>>Fevereiro</option>
+            <option value="03" <?=($_GET['filtroMes']  == '03')?'selected':''?>>Março</option>
+            <option value="04" <?=($_GET['filtroMes']  == '04')?'selected':''?>>Abril</option>
+            <option value="05" <?=($_GET['filtroMes']  == '05')?'selected':''?>>Maio</option>
+            <option value="06" <?=($_GET['filtroMes']  == '06')?'selected':''?>>Junho</option>
+            <option value="07" <?=($_GET['filtroMes']  == '07')?'selected':''?>>Julho</option>
+            <option value="08" <?=($_GET['filtroMes']  == '08')?'selected':''?>>Agosto</option>
+            <option value="09" <?=($_GET['filtroMes']  == '09')?'selected':''?>>Setembro</option>
+            <option value="10" <?=($_GET['filtroMes']  == '10')?'selected':''?>>Outubro</option>
+            <option value="11" <?=($_GET['filtroMes']  == '11')?'selected':''?>>Novembro</option>
+            <option value="12" <?=($_GET['filtroMes']  == '12')?'selected':''?>>Dezembro</option>
         </select>
     </div>
     <button class="relatorio-a" type="submit" onClick="fazerRelatorio()"><i class="fas fa-search"></i></button>
